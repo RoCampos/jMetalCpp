95ea5e4bb49c7a45d173d1a599cfe39d04a4a019
Romerito Campos <romerito.campos@gmail.com>
2017-08-07 16:53:20 -0300

    New problem has been added

--- src/problems/romerito/mmrp.cpp
+++ src/problems/romerito/mmrp.cpp
@@ -1,12 +1,14 @@
 #include <mmrp.h>
 
 
+
+
 MMRP::MMRP(std::string)
 {
 	//default constructor
 }
 
-MMRP::MMRP (std::string solutionType, std::string instance)
+MMRP::MMRP (std::string solutionType, std::string instance, std::string conf)
 {
 	//solution type do not reprensent the variables
 	numberOfVariables_ = 0;
@@ -22,13 +24,23 @@ MMRP::MMRP (std::string solutionType, std::string instance)
 
 	//defined in Problem.h
 	//It uses my representation (wrapper)
+	//Inicialização ocorre aqui
 	solutionType_ = new EdgeSolutionType (this);
+
+	//configurando rede e grupos
+	rca::reader::MultipleMulticastReader reader(instance);
+	network_ = new rca::Network;
+	reader.readerGroup (mgroups_);
+	reader.configNetwork (network_);
 
 }
 
 MMRP::~MMRP()
 {
-
+	for (int i = 0; i < mgroups_.size (); ++i)
+	{
+		delete mgroups_[i];
+	}
 
 
 }

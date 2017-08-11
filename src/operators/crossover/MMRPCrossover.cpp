289206dc50cc0a1434459bb8cbb4eb237e4c4ec6
Romerito Campos <romerito.campos@gmail.com>
2017-08-11 16:44:56 -0300

    crossover operator has been added

--- src/operators/crossover/MMRPCrossover.cpp
+++ src/operators/crossover/MMRPCrossover.cpp
@@ -10,15 +10,19 @@ MMRPCrossover::MMRPCrossover(map<string, void *> parameters)
 
 void * MMRPCrossover::execute (void* object) {
 
+	std::cout << __FUNCTION__ << std::endl;
+
 	void ** parameters = (void **) object;
 
 	Solution * current = (Solution *) parameters[0];
 	Solution ** parent = (Solution **) parameters[1];
 
+	MMRP * mmrp = (MMRP *) current->getProblem();
+
 	Solution * child = new Solution (current->getNumberOfObjectives ());
 
-	Individual &ind1 = ((EdgeSolutionType*) parent[0]->getType ())->get_representation();
-	Individual &ind2 = ((EdgeSolutionType*) parent[1]->getType ())->get_representation();
+	Individual &ind1 = parent[0]->get_representation();
+	Individual &ind2 = parent[1]->get_representation();
 	Individual novo; 
 
 	int CSIZE = ind1.cromossoma.size ();
@@ -27,11 +31,14 @@ void * MMRPCrossover::execute (void* object) {
 	novo.cost = std::vector<int> (CSIZE);
 
 	//Applying crossover on the solution
-	crossover_by_tree (ind1, ind2, novo);
+	crossover_by_hop (ind1, ind2, novo, 30);
 
-	EdgeSolutionType * est = new EdgeSolutionType (current->getProblem ());
-	est->set_representation (novo);
-	child->setType (est);
+	evaluate (novo, 
+		*mmrp->get_network(), 
+		mmrp->get_groups());
+
+	child->set_representation (novo);
+	child->setType (current->getProblem()->getSolutionType());
 
 	return child;
 
